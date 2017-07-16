3403025f2465464828949ba8aec7d7bd6c8050ab
Ben Windsor <benwindsor@gmail.com>
2017-07-16 09:28:47 +0100

    Update travis to include mysql

--- src/data/credentials.ts
+++ src/data/credentials.ts
@@ -3,20 +3,17 @@ import * as fs from 'fs';
 
 let content = fs.readFileSync(process.env.TEST_ENVIRONMENT=="1"?'./credentials_template.json':'./credentials.json');
 let credentials: Credentials = JSON.parse(content.toString());
-if (!credentials.mysql) {
-    credentials = {
-        ...credentials,
-        mysql: {
-            host: process.env.RDS_HOSTNAME,
-            db_name: process.env.RDS_DB_NAME,
-            user: process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            adminuser: "",
-            adminpass: "",
-            port: Number.parseInt(process.env.RDS_PORT)
-        }
+// Override with anything set in the environment
+credentials = {
+    ...credentials,
+    mysql: {
+        host: process.env.RDS_HOSTNAME || credentials.mysql.host,
+        db_name: process.env.RDS_DB_NAME || credentials.mysql.db_name,
+        user: process.env.RDS_USERNAME || credentials.mysql.user,
+        password: process.env.RDS_PASSWORD || credentials.mysql.password,
+        adminuser: process.env.RDS_ROOT_USER || credentials.mysql.adminuser,
+        adminpass: process.env.RDS_ROOT_PASS || credentials.mysql.adminpass,
+        port: Number.parseInt(process.env.RDS_PORT) || credentials.mysql.port || 3306
     }
-} else if (!credentials.mysql.port) {
-    credentials.mysql.port = 3306
 }
 export default credentials
