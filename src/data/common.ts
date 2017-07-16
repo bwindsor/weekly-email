0a9c11054b509aa577f3b06385d2b9c41b4b74c2
Ben Windsor <benwindsor@gmail.com>
2017-07-16 09:45:11 +0100

    Try no password option

--- src/data/common.ts
+++ src/data/common.ts
@@ -5,6 +5,19 @@ import credentials from './credentials'
 export const TABLE_NAME = process.env.TABLE_NAME || ((process.env.TEST_ENVIRONMENT=="1")?"trainings_test": "trainings");
 export var pool : mysql.IPool;
 
+let poolOptions = {
+  host     : credentials.mysql.host,
+  user     : credentials.mysql.user,
+  database : credentials.mysql.db_name,
+  port     : credentials.mysql.port
+}
+if (credentials.mysql.password.length > 0) {
+  poolOptions = {
+    ...poolOptions,
+    password : credentials.mysql.password
+  }
+}
+
 pool  = mysql.createPool({
   host     : credentials.mysql.host,
   user     : credentials.mysql.user,
