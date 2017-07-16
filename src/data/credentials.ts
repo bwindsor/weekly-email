b9ae18122daa67fd29fa99fecb1435fa7a697ea1
Ben Windsor <benwindsor@gmail.com>
2017-07-16 10:20:39 +0100

    Changed how blank but set env vars are processed

--- src/data/credentials.ts
+++ src/data/credentials.ts
@@ -7,13 +7,22 @@ let credentials: Credentials = JSON.parse(content.toString());
 credentials = {
     ...credentials,
     mysql: {
-        host: process.env.RDS_HOSTNAME || credentials.mysql.host,
-        db_name: process.env.RDS_DB_NAME || credentials.mysql.db_name,
-        user: process.env.RDS_USERNAME || credentials.mysql.user,
-        password: process.env.RDS_PASSWORD || credentials.mysql.password,
-        adminuser: process.env.RDS_ROOT_USER || credentials.mysql.adminuser,
-        adminpass: process.env.RDS_ROOT_PASS || credentials.mysql.adminpass,
+        host: parseEnv('RDS_HOSTNAME', credentials.mysql.host),
+        db_name: parseEnv('RDS_DB_NAME', credentials.mysql.db_name),
+        user: parseEnv('RDS_USERNAME', credentials.mysql.user),
+        password: parseEnv('RDS_PASSWORD', credentials.mysql.password),
+        adminuser: parseEnv('RDS_ROOT_USER', credentials.mysql.adminuser),
+        adminpass: parseEnv('RDS_ROOT_PASS', credentials.mysql.adminpass),
         port: Number.parseInt(process.env.RDS_PORT) || credentials.mysql.port || 3306
     }
 }
 export default credentials
+
+function parseEnv(varName, defaultValue) {
+    let v = process.env[varName]
+    if (v === undefined || v==='') {
+        return v
+    } else {
+        return defaultValue
+    }
+}
