ea01a56e1396e430b7d675cd9ede4852a873b9e2
Ben Windsor <benwindsor@gmail.com>
2017-07-16 09:50:20 +0100

    Define type of pooloptions

--- src/data/common.ts
+++ src/data/common.ts
@@ -5,12 +5,13 @@ import credentials from './credentials'
 export const TABLE_NAME = process.env.TABLE_NAME || ((process.env.TEST_ENVIRONMENT=="1")?"trainings_test": "trainings");
 export var pool : mysql.IPool;
 
-let poolOptions = {
+let poolOptions:mysql.IPoolConfig = {
   host     : credentials.mysql.host,
   user     : credentials.mysql.user,
   database : credentials.mysql.db_name,
   port     : credentials.mysql.port
 }
+
 if (credentials.mysql.password.length > 0) {
   poolOptions = {
     ...poolOptions,
