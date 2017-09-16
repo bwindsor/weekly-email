b2645ffef262d5aa8b7dbcce59dc0788c599061c
Ben Windsor <benwindsor@gmail.com>
2017-09-16 23:27:41 +0100

    Fix typescript error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-let port = process.env.PORT || 3000
+let port = parseInt(process.env.PORT) || 3000
 
 app.listen(port, 'localhost', () => {
     console.log("Listening on port " + port)
